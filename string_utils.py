d541113343198d4b12f512b860ce7edb44d18d83
324235258 <noalow1@gmail.com>
2025-11-22 15:19:15 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,6 +1,4 @@
 
-
-
 def split_before_uppercases(formula):
     if not formula:
         return []
@@ -40,13 +38,13 @@ def count_atoms_in_molecule(molecular_formula):
     return atom_counts
 
 
-
 def parse_chemical_reaction(reaction_equation):
     """Takes a reaction equation (string) and returns reactants and products as lists.  
     Example: 'H2 + O2 -> H2O' → (['H2', 'O2'], ['H2O'])"""
     reaction_equation = reaction_equation.replace(" ", "")  # Remove spaces for easier parsing
     reactants, products = reaction_equation.split("->")
     return reactants.split("+"), products.split("+")
+
 
 def count_atoms_in_reaction(molecules_list):
     """Takes a list of molecular formulas and returns a list of atom count dictionaries.  
