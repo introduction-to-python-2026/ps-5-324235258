5f9330c5f6897b397934efae00945ea821e00655
324235258 <noalow1@gmail.com>
2025-11-26 18:26:14 +0200

    Reformat import statements for readability

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from string_utils import (parse_chemical_reaction,  count_atoms_in_reaction,  count_atoms_in_molecule, split_before_uppercases, split_at_digit)
-
+from string_utils import (
+    parse_chemical_reaction,
+    count_atoms_in_reaction,
+    count_atoms_in_molecule,
+    split_before_uppercases,
+    split_at_digit
+)
 from equation_utils import build_equations, my_solve
 
 def balance_reaction(reaction): #"Fe2O3 + H2 -> Fe + H2O"
