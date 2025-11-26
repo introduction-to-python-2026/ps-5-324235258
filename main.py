22a40cd591bec06452845fd418902a43409fc50f
324235258 <noalow1@gmail.com>
2025-11-26 18:23:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import importlib, string_utils, equation_utils
-importlib.reload(string_utils)
-importlib.reload(equation_utils)
-# Add the import statements for functions from string_utils.py and equation_utils.py here
+from string_utils import (parse_chemical_reaction,  count_atoms_in_reaction,  count_atoms_in_molecule, split_before_uppercases, split_at_digit)
+
+from equation_utils import build_equations, my_solve
 
 def balance_reaction(reaction): #"Fe2O3 + H2 -> Fe + H2O"
 
